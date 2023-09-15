9870c8917eaa970288d2f80a1d078e7fa7f5ab50
artyomkoltunov <test@test.test>
2023-09-15 18:46:52 +0300

    Modals to components; install pinia

--- delievery-food/src/App.vue
+++ delievery-food/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-  import BaseHeader from '@/components/BaseHeader.vue'
-  import BaseFooter from '@/components/BaseFooter.vue'
+import BaseHeader from '@/components/BaseHeader.vue'
+import BaseFooter from '@/components/BaseFooter.vue'
+import CartPopup from '@/components/CartPopup.vue'
+import LoginPopup from '@/components/LoginPopup.vue'
 </script>
 
 <template>
   <base-header />
   <router-view/>
   <base-footer />
+  <cart-popup />
+  <login-popup />
 </template>
 
 <style>
