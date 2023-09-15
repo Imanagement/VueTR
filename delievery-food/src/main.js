5310faf7fd80921371028da812aba4d130e6cc26
artyomkoltunov <test@test.test>
2023-09-15 21:24:15 +0300

    Pizza items order functionality

--- delievery-food/src/main.js
+++ delievery-food/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
 const pinia = createPinia()
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App)
+    .use(router)
+    .use(pinia)
+    .use(ElementPlus)
+    .mount('#app')
--- delievery-food/src/views/RestaurantView.vue
+++ delievery-food/src/views/RestaurantView.vue
@@ -1,20 +1,37 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRoute, useRouter} from 'vue-router'
 import { pizzas, restaurants } from '@/data'
 import ProductCardItem from '@/components/ProductCardItem.vue'
 
+const orderOptions = [
+  {
+    value: 'cost-less',
+    label: "Подешевле",
+  },
+  {
+    value: 'cost-more',
+    label: "Подороже",
+  }
+]
+const sort = ref()
+
 const restaurant = ref()
+
 onMounted(() => {
   const route = useRoute()
   const router = useRouter()
   const restaurantID = route.params.id
-  console.log(restaurants)
-  console.log(restaurantID, "ID")
   const filter = restaurants.filter(restaurant => String(restaurant.id) === String(restaurantID))
-  console.log(filter)
   if (!filter.length) router.push("/")
   restaurant.value = filter[0]
+})
+
+const getPizzas = computed(() => {
+  if (!sort.value) return pizzas
+  const result = pizzas.sort((a, b) => a.price - b.price)
+  if (sort.value !== 'cost-more') return result
+  return result.reverse()
 })
 
 </script>
@@ -23,18 +40,23 @@ onMounted(() => {
 		<div class="container">
 			<section class="menu">
 				<div class="section-heading">
-					<h2 class="section-title restaurant-title">{{ restaurant.name }}</h2>
-					<div class="card-info">
-						<div class="rating">
-							{{ restaurant.rating }}
-						</div>
-						<div class="price">От {{ restaurant.price }} ₽</div>
-						<div class="category">{{ restaurant.category.name }}</div>
-					</div>
+          <div class="section-heading-title-info">
+            <h2 class="section-title restaurant-title">{{ restaurant.name }}</h2>
+            <div class="card-info">
+          <div class="rating">
+            {{ restaurant.rating }}
+          </div>
+          <div class="price">От {{ restaurant.price }} ₽</div>
+          <div class="category">{{ restaurant.category.name }}</div>
+        </div>
+          </div>
+          <div class="section-ordering">
+            <el-select-v2 v-model="sort" :options="orderOptions"/>
+          </div>
 					<!-- /.card-info -->
 				</div>
 				<div class="cards cards-menu">
-          <product-card-item v-for="pizza in pizzas" :key="pizza.id" :product="pizza" />
+          <product-card-item v-for="pizza in getPizzas" :key="pizza.id" :product="pizza" />
 				</div>
 				<!-- /.cards -->
 			</section>
@@ -42,3 +64,12 @@ onMounted(() => {
 		<!-- /.container -->
 	</main>
 </template>
+
+<style scoped>
+.section-heading-title-info {
+  display: flex;
+}
+.section-heading {
+  justify-content: space-between;
+}
+</style>
